e19f1d990f04a499f120fc124aebe1497de8dcba
MurHyun2 <eddy152264@gmail.com>
2025-10-17 18:18:10 +0900

    섹션4.Clock을 이용한 시간 테스트

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -7,24 +7,13 @@ import tobyspring.hellospring.payment.PaymentService;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 
 public class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(PaymentConfig.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println("Payment: " + payment);
-        System.out.println("--------------------------------------------\n");
-
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment2: " + payment);
-        System.out.println("--------------------------------------------\n");
-
-        TimeUnit.SECONDS.sleep(3);
-
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment3: " + payment);
     }
 }
