fc39d73fd83d6438aa4898418a4a1d922d7a270b
MurHyun2 <eddy152264@gmail.com>
2025-10-17 14:55:24 +0900

    섹션3.DI와 디자인 패턴

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,13 +5,24 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
+        System.out.println("Payment: " + payment);
+        System.out.println("--------------------------------------------\n");
+
+        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment2: " + payment);
+        System.out.println("--------------------------------------------\n");
+
+        TimeUnit.SECONDS.sleep(3);
+
+        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment3: " + payment);
     }
 }
