486464c7f13e8dc704ce9361ac466b9cec60d81f
MurHyun2 <eddy152264@gmail.com>
2025-10-17 17:36:51 +0900

    섹션4.테스트와 DI

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -1,34 +1,39 @@
 package tobyspring.hellospring.payment;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.*;
 import static org.assertj.core.api.Assertions.*;
 
 class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
-    void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
-
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
-
-        // 환율 정보를 가져온다
-        assertThat(payment.getExRate()).isNotNull();
-
-        // 원화 환산 금액 계산
-        assertThat(payment.getConvertedAmount()).isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
+    void convertedAmount() throws IOException {
+        getPayment(valueOf(500), valueOf(5_000));
+        getPayment(valueOf(1_000), valueOf(10_000));
+        getPayment(valueOf(3_000), valueOf(30_000));
 
         // 원화 환산 금액 유효시간 계산
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
 
+    }
+
+    private static void getPayment(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", TEN);
+
+        // 환율 정보를 가져온다
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+
+        // 원화 환산 금액 계산
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
