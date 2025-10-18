6d5a9a6d7665bb9a2e1382686c2d46a676125f31
MurHyun2 <eddy152264@gmail.com>
2025-10-18 14:09:35 +0900

    섹션4.도메인 오브젝트 테스트

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentTest.java
@@ -0,0 +1,44 @@
+package tobyspring.hellospring.payment;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class PaymentTest {
+
+    private Clock clock;
+    private ExRateProvider exRateProvider;
+
+    @BeforeEach
+    void beforeEach() {
+        this.clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+        this.exRateProvider = new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+
+    @Test
+    void createPrepared() throws IOException {
+        Payment payment = Payment.createPrepared(
+                1L, "USD", BigDecimal.TEN, exRateProvider, LocalDateTime.now(clock)
+        );
+
+        Assertions.assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(LocalDateTime.now(clock).plusMinutes(30));
+    }
+
+    @Test
+    void isValid() throws IOException {
+
+        Payment payment = Payment.createPrepared(
+                1L, "USD", BigDecimal.TEN, exRateProvider, LocalDateTime.now(clock)
+        );
+
+        Assertions.assertThat(payment.isValid(clock)).isTrue();
+        Assertions.assertThat(payment.isValid(Clock.offset(clock, Duration.of(30, ChronoUnit.MINUTES)))).isFalse();
+    }
+}
